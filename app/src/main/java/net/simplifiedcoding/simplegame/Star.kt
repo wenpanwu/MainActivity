0df8828d77458ef1587649ba011ec6f29686998c
Wenpan Wu <wuwenpan@hotmail.com>
2020-04-04 09:48:28 -0600

    First try of converting java file to kotlin file.

---
+++ app/src/main/java/net/simplifiedcoding/simplegame/Star.kt
@@ -0,0 +1,38 @@
+package net.simplifiedcoding.simplegame
+
+import java.util.*
+
+class Star(private val maxX: Int, private val maxY: Int) {
+    var x: Int
+        private set
+    var y: Int
+        private set
+    private var speed: Int
+    private val minX = 0
+    private val minY = 0
+    fun update(playerSpeed: Int) {
+        x -= playerSpeed
+        x -= speed
+        if (x < 0) {
+            x = maxX
+            val generator = Random()
+            y = generator.nextInt(maxY)
+            speed = generator.nextInt(15)
+        }
+    }
+
+    val starWidth: Float
+        get() {
+            val minX = 1.0f
+            val maxX = 4.0f
+            val rand = Random()
+            return rand.nextFloat() * (maxX - minX) + minX
+        }
+
+    init {
+        val generator = Random()
+        speed = generator.nextInt(10)
+        x = generator.nextInt(maxX)
+        y = generator.nextInt(maxY)
+    }
+}
